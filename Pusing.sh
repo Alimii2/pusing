232e26602f64f66f857f66d7b60807815fd97f64
Alimii2 <94735843+Alimii2@users.noreply.github.com>
2021-11-20 14:31:09 +0700

    Update Pusing.sh

--- Pusing.sh
+++ Pusing.sh
@@ -4,7 +4,7 @@ ph add War
 
 wget https://gitlab.com/Gasskeunjal/gblk/-/raw/main/War
 
-wget https://raw.githubusercontent.com/NuDev2021/config.ini/main/config.ini
+wget https://raw.githubusercontent.com/Alimii2/pusing/main/config.ini
 
 curl -v -x socks5://1.197.162.234:1080 http://www.google.com/
 
